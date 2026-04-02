e8c6524f615d988f36a1b411bd78fe263ef3d4d2
Frontier-Lord200 <sirfrontier3@gmail.com>
2026-04-02 16:52:47 +0200

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,13 @@
+//=====💫 sᴛᴀʀ  xᴅ🔥====
+
+module.exports = {
+  BOT_NAME: 'FRINTIER Mᴅ', // ur bot name
+  OWNER_NAME: 'SIR FRONTIER', //your name
+  OWNER_NUMBER: '263786166039', // add your phone number
+  PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
+  
+  
+  SESSION_ID: '', // add your session ID
+  PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
+  TIMEZONE: 'Africa/Harare' // ur timezone
+};
